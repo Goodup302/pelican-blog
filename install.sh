4160c2e90e9461ce1bd277347ea4dff573d9c658
Goodup302 <j.f0471430704@gmail.com>
2019-12-20 14:21:40 +0100

    add Pelican

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@ apt-get update -y
 apt-get upgrade -y
 
 apt-get install -y python-virtualenv python-dev python-pip
-apt-get install -y nano vim pelican
+apt-get install -y pelican markdown
 
-pip install pelican Markdown typogrify
+pip install typogrify
 
 apt-get remove -y docker docker-engine docker.io
 apt install -y docker.io
